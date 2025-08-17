ef33bdae3952267cd9fa6c31c43cf48404779a65
mohammedabdulhamid1108 <143341179+mohammedabdulhamid1108@users.noreply.github.com>
2025-08-17 10:32:15 +0300

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,5 @@
+let birthYear = prompt("Enter your birth year:11/08/1994");
+let currentYear = new Date().getFullYear(06/28/2025);
+let age = currentYear - birthYear;
+
+alert("You are " + age + " years old.");
